fd042d9f2ddfb9fa071e0ade458c8db64815ec52
Vitor Boccio <vitorboccio@gmail.com>
2024-01-24 23:12:56 -0700

    pipeline fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { createClient } from "contentful";
 import logo from "../assets/logo.svg";
 import { GetInTouch } from "./GetInTouch";
