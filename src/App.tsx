d190452788924009ab5225c6073d2b72e1ebde6c
Vitor Boccio <vitorboccio@gmail.com>
2025-09-25 11:03:57 -0600

    update contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { FeatureFlagProvider } from "@flagpole/react";
 import "./App.css";
 import { About } from "./components/About";
 import { Admission } from "./components/Admission";
@@ -11,7 +12,7 @@ import ReactGA from "react-ga4";
 ReactGA.initialize(import.meta.env.VITE_GA_MEASUREMENT_ID);
 function App() {
   return (
-    <>
+    <FeatureFlagProvider apiKey="fp_live_e9cb4c83a08a2c176373919d495960a08e13f3eace6435e0">
       <Hero />
       <div className="common_style">
         <About />
@@ -21,7 +22,7 @@ function App() {
       <Testemonial />
       <Contact />
       <Footer />
-    </>
+    </FeatureFlagProvider>
   );
 }
 
